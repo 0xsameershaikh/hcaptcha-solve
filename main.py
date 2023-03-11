c49a22de022533d5172d02bec7523724b243f7ab
0xsameershaikh <127571847+0xsameershaikh@users.noreply.github.com>
2023-03-11 08:38:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+import time
+
+
+
+def getcaptcha(apikey,sitekey,url):
+
+    url = f"http://2captcha.com/in.php?key={apikey}&method=hcaptcha&sitekey={sitekey}&pageurl={url}"
+    response = requests.get(url)
+    if "|" not in response.text:
+        print(f"could not create captcha task: {response.text}")
+        exit()
+    else:
+        task_id = response.text.split("|")[1]
+    for x in range(0,30):
+        time.sleep(5)
+        url = f"http://2captcha.com/res.php?key={apikey}&action=get&id={task_id}"
+        response = requests.get(url)
+
+        if response.text != "CAPCHA_NOT_READY":
+            return response.text.replace("OK|", "")
+
+    return None
+
+
+
+
+if __name__ == '__main__':
+    APIKEY=""
+    SITEKEY =""
+    URL =""
+
+    getcaptcha(APIKEY,SITEKEY,URL)
